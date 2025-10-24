9a5f1a9f68cc2aa8cec705a1fc8e1082b8ff7c78
twinklefins <yujoo.seoul@gmail.com>
2025-10-24 16:12:10 +0900

    print correct

--- main/main.py
+++ main/main.py
@@ -1,1 +1,10 @@
-print("안녕")
+import streamlit as st
+import numpy as np
+
+def main():
+
+    st.markdown("# Hello Strealit")
+    st.write(np.__version__)
+
+if __name__ == "__main__":
+    main()
